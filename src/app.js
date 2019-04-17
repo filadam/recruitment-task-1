aad99d9fb53515645c0a9d8453536819b964416c
filadam <filip.adamczyk90@gmail.com>
2019-04-17 14:12:12 +0200

    changes to structure

--- src/app.js
+++ src/app.js
@@ -1,11 +1,11 @@
 import React from 'react';
 
 import './index.css';
-import Header from './components/layout/header/header'
-import Carousel from './components/layout/carousel/carousel'
-import About from './components/layout/aboutme/aboutme'
-import InTouch from './components/layout/intouch/intouch'
-import Footer from './components/layout/footer/footer'
+import Header from './components/landingpage/layout/header/header'
+import Carousel from './components/landingpage/layout/carousel/carousel'
+import About from './components/landingpage/layout/aboutme/aboutme'
+import InTouch from './components/landingpage/layout/intouch/intouch'
+import Footer from './components/landingpage/layout/footer/footer'
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 class App extends React.Component {
